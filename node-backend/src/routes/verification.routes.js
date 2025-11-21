0fa3f5af04c69167fc2bd6e1b4a6bfedd9289504
ayushsingh212 <ayurajput249@gmail.com>
2025-11-21 16:02:30 +0530

    Added the otp verification route

--- node-backend/src/routes/verification.routes.js
+++ node-backend/src/routes/verification.routes.js
@@ -1,12 +1,12 @@
 import {Router} from "express"
-import { sendOTP } from "../controllers/verification.controller.js";
+import { checkOtp, sendOTP } from "../controllers/verification.controller.js";
 
 
 const router = Router();
 
 
 router.post("/getOtp/:purpose",sendOTP);
-
+router.post("/verifyOtp",checkOtp)
 
 
 export default router;
